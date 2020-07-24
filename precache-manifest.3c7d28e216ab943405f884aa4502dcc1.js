b4de3e8af687f0d8b88525bd6e53b4605cfc1aff
Manobala S <manobala.s@Hotmail.com>
2020-07-24 19:37:50 +0530

    init2

---
+++ precache-manifest.3c7d28e216ab943405f884aa4502dcc1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bca8f744aa84f24cbedda0d123c69c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f07239d9bd797a389057",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9925b16b48d336df1c41",
+    "url": "/static/js/2.b279f13b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b279f13b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f07239d9bd797a389057",
+    "url": "/static/js/main.e1b5fddf.chunk.js"
+  },
+  {
+    "revision": "c226879bc64cba6d84a3",
+    "url": "/static/js/runtime-main.453361e1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
