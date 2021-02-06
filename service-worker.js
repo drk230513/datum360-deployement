cae833f6fb58d329b11af31eca80b02c4d0ed8e9
priyanka m <priyankamsp.15@gmail.com>
2021-02-06 18:59:17 +0530

    vignesh -

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4249b6af528ee666c609bbccb912b342.js"
+  "/precache-manifest.90449f7ae49d3d883bc8a1a8c14ed60e.js"
 );
 
 self.addEventListener('message', (event) => {
