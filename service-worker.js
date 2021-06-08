7fdcb72e350cf49f0e4c5747b5ba485ab11c0b0a
vignesh <vigneshthandapani19@gmail.com>
2021-06-08 10:18:14 +0530

    edit attributes build

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.100a337b8f26521449ac7b961cfcc8c2.js"
+  "/precache-manifest.7e5c123375432a1ca2f0d53b72ecb152.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
