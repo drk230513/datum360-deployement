c579f114f82fba5b35ccbb9022b036c8c13416a3
vignesh <vigneshthandapani19@gmail.com>
2021-07-31 19:17:08 +0530

    search build

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.81e2d2afe912baedfefa092592a3c795.js"
+  "/precache-manifest.5626a11a1ed176af72ac9675576f3426.js"
 );
 
 self.addEventListener('message', (event) => {
